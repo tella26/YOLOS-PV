91d62952e89d21df97303e538338773dadb42b55
Tella babatunde <30381435+tella26@users.noreply.github.com>
2023-04-11 15:02:15 +0300

    update

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ def main(args):
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
             args.start_epoch = checkpoint['epoch'] + 1
-
+    
     if args.eval:
         test_stats, coco_evaluator = evaluate(model, criterion, postprocessors,
                                               data_loader_val, base_ds, device, args.output_dir, args.weighted_l1, args.weighted_giou)
